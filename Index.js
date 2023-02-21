3e301a5fa4c99ab7b68f8779a5fa0f60faa9d91f
Your NameKyle <kylemcb.24@gmail.com>
2023-02-21 10:50:33 +0200

    test

--- Index.js
+++ Index.js
@@ -4,19 +4,17 @@ jQuery(function () {
 	
 
 	$('.car').on('click', '.next-slide', function () {
-		
-		 $('.carousel-dots .carousel-dot').find('active').removeClass('active');
-		 $('.controls .carousel-dot .carousel-dots').eq($(this).attr('rel')).addClass('active');
-		
-		 $(this).addClass('active');
-	
+		console.log($(".slide-container"))
+
 		if ($('.car').find('.slide-container.active').next().length) {
 			$('.car').find('.slide-container.active').removeClass('active').next('.slide-container').addClass('active')
+			
+			
 		} else {
 			$('.car').find('.slide-container.active').removeClass('active')
 			$('.car').find('.slide-container:first-child').addClass('active');
 		}
-
+		$('.car .slides .slide-container ').eq($(this).attr('rel')).addClass('active');
 		var rel = $('.car').find('.slide-container.active').attr('rel');
 		console.log(rel);
 	});
@@ -24,7 +22,7 @@ jQuery(function () {
 $(document).ready(function () {
 	    $(".carousel-dot").on('click', function () {
 		$('.car').find('.active').removeClass('active');
-		$('.car .slides .slide-container').eq($(this).attr('rel')).addClass('active');
+		$('.car .slides .slide-container ').eq($(this).attr('rel')).addClass('active');
 		$('.carousel-dots').find('active').removeClass('active');
 		$(this).addClass('active');
 	});
